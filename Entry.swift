f93aa77d049b6778f7ff142d0f4056c14ca58754
Mike Gilroy <mjgilroy@me.com>
2015-10-16 15:32:37 -0600

    added failable init,
    and private var

--- Entry.swift
+++ Entry.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
+
+
 class Entry: Equatable {
+    private let titleKey = "title"
+    private let bodyTextKey = "bodyText"
+    private let timestampKey = "timestamp"
+    
 //    var timestamp = NSDate
     var timestamp: NSDate
     var title: String
@@ -19,6 +25,27 @@ class Entry: Equatable {
         self.bodyText = title
         self.timestamp = timestamp
     }
+    init? (dictionary: [String:AnyObject]){
+        guard let titleFromDictonary = dictionary["title"] as? String, let bodyTextFromDictonary = dictionary["bodyText"] as? String, let timestampFromDictionary = dictionary ["timestamp"] as? NSDate else{
+            self.title = ""
+            self.bodyText = ""
+            self.timestamp = NSDate()
+            return nil
+        }
+        self.title = titleFromDictonary
+        self.bodyText = bodyTextFromDictonary
+        self.timestamp = timestampFromDictionary
+    }
+    func dictionaryRepresentation() ->[String: AnyObject]{
+        
+        return[
+            "title" : self.title,
+            "artist" : self.bodyText,
+            "timestamp" : self.timestamp
+            
+        ]
+    }
+    
 }
 
 func ==(lhs: Entry, rhs: Entry) -> Bool {
